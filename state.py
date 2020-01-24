f334f7f8e922e27110417444d1ff98ac8e2fb99e
Zaimonius <simongus1@hotmail.com>
2020-01-24 19:22:52 +0100

    message begin + game loop complete

--- state.py
+++ state.py
@@ -1,4 +1,7 @@
 import time
+import random
+import enum
+import asyncio
 
 class State:
     def Enter(self):
@@ -16,7 +19,7 @@ class Entity:
 
     def setID(self,value):
         self.id = value
-        print("Entity id set")
+        print("Entity id set " + str(value))
 
     def __init__(self,idNumber):
         self.setID(idNumber)
@@ -67,85 +70,88 @@ class StateMachine:
 class DigForNuggs(State):
     def Enter(self,miner):
         if miner.location != "goldmine":
-            print("no goldmine, aww man \n")
+            print("[Miner]no goldmine, aww man")
             miner.location = "goldmine"
     
     def Execute(self,miner):
-        miner.goldCarried += 1
-        print("one more nugget weoo\n")
-        miner.fatigue += 2
-        miner.thirst += 5
-        if miner.goldCarried == 10:
+        miner.goldCarried += 2
+        print("[Miner]one more nugget weoo")
+        miner.fatigue += 5
+        miner.thirst += 10
+        if miner.goldCarried > 10:
             miner.changeState(VisitBank())
-            print("gotta go bank")
-        elif miner.thirst == 10:
+            print("[Miner]gotta go bank")
+        elif miner.thirst > 100:
             miner.changeState(QuenchThirst())
-            print("i need me slurp")
-        elif miner.fatigue == 10:
+            print("[Miner]i need me slurp")
+        elif miner.fatigue > 100:
             miner.changeState(GoSleep())
-            print("im tired!")
+            print("[Miner]im tired!")
     def Exit(self,miner):
-        print("leaving the digsite\n")
+        print("[Miner]leaving the digsite")
 
 class VisitBank(State):
     def Enter(self,miner):
             if miner.location != "bank":
-                print("going to bank!\n")
+                print("[Miner]going to bank!")
                 miner.location = "bank"
         
     def Execute(self,miner):
         miner.moneyInBank += miner.goldCarried
         miner.goldCarried = 0
-        if miner.fatigue == 10:
+        if miner.fatigue == 100:
             miner.changeState(GoSleep())
-            print("im tired!")
+            print("[Miner]im tired!")
         else:
             miner.changeState(DigForNuggs())
-            print("back to work")
+            print("[Miner]back to work")
         
     def Exit(self,miner):
-        if miner.fatigue == 10:
-            print("going home to sleep\n")
-        else:
-            print("back to work \n")
+        print("[Miner]leaving bank naow")
         
 class QuenchThirst(State):
     def Enter(self,miner):
         if miner.location != "saloon":
-            print("gotta quench the thirst")
+            print("[Miner]gotta quench the thirst")
             miner.location = "saloon"
 
     def Execute(self,miner):
         miner.thirst = 0
-        print("ahhhhh")
+        print("[Miner]ahhhhh")
         if miner.fatigue == 10:
             miner.changeState(GoSleep())
         else:
             miner.changeState(DigForNuggs())
         
     def Exit(self,miner):
-        if miner.fatigue == 10:
-            print("im tired, going home")
-        else:
-            print("ohhhhh \n")
+        print("[Miner]Whisky sure is nice")
         
 class GoSleep(State):
     def Enter(self,miner):
         if miner.location != "home":
             miner.location = "home"
-            print("going home")
+            print("[Miner]going home")
         else:
-            print("going to sleep")
+            print("[Miner]going to sleep")
     
     def Execute(self,miner):
         miner.fatigue = 0
-        print("zzz")
-        miner.changeState(DigForNuggs())
-        
+        print("[Miner]zzz")
+        if miner.thirst > 10:
+            miner.changeState(QuenchThirst())
+        else:
+            miner.changeState(DigForNuggs())
+
     def Exit(self,miner):
-        print("Back to the mine!")
+        print("[Miner]Back to the mine!")
 
 class Miner(Entity):
+    # States
+    #-----------------
+    # DigForNuggs
+    # VisitBank
+    # QuenchThirst
+    # GoSleep
     def __init__(self,IDValue):
         self.setID(IDValue)
         self.stateMachine = StateMachine(self)
@@ -157,15 +163,136 @@ class Miner(Entity):
         self.fatigue = 0
 
     def Update(self):
-        self.thirst += 1
         self.stateMachine.Update()
 
     def changeState(self,newState):
         self.stateMachine.ChangeState(newState)
 
+class MakeBed(State):
+    def Enter(self,houseWife):
+        print("[Wife]time to make the bed!")
+    
+    def Execute(self,houseWife):
+        print("[Wife]making the bed...")
+        houseWife.changeState(HouseWork())
+        
+    def Exit(self,houseWife):
+        print("[Wife]bed made!")
+
+class MopFloor(State):
+    def Enter(self,houseWife):
+        print("[Wife]time to mop the floor!")
+    
+    def Execute(self,houseWife):
+        print("[Wife]mopping the floor...")
+        houseWife.changeState(HouseWork())
+
+        
+    def Exit(self,houseWife):
+        print("[Wife]floor mopped!")
+    
+class GoToBathroom(State):
+    def Enter(self,houseWife):
+        print("[Wife]need to use the ladies room")
+    
+    def Execute(self,houseWife):
+        print("[Wife]taking dump...")
+        houseWife.changeState(HouseWork())
+        
+    def Exit(self,houseWife):
+        print("[Wife]dump complete!")
+
+class HouseWork(State):
+    def Enter(self,houseWife):
+        print("[Wife]time to the next chore!")
+    
+    def Execute(self,houseWife):
+        chore = random.randint(0,1)
+        toilet = random.randint(0,10)
+        if toilet == 1:
+            houseWife.changeState(GoToBathroom())
+        if chore == 0:
+            houseWife.changeState(MakeBed())
+        else:
+            houseWife.changeState(MopFloor())
+        
+    def Exit(self,houseWife):
+        print("[Wife]Phew!")
+
+class HouseWife(Entity):
+    # States
+    #-----------------
+    # HouseWork
+    # - MakeBed
+    # - MopFloor
+    # GoToBathroom
+
+    def __init__(self,IDValue):
+        self.setID(IDValue)
+        self.stateMachine = StateMachine(self)
+        self.stateMachine.setCurrentState(HouseWork())
+    
+    def Update(self):
+        self.stateMachine.Update()
+
+    def changeState(self,newState):
+        self.stateMachine.ChangeState(newState)
+
+class MessageType(enum.Enum):
+    Msg_HoneyImHome = "imh"
+    Msg_StewReady = "swr"
+
+class Telegram:
+    def __init__(self,sender,receiver,messagetype,dispatchtime,extrainfo):
+        self.sender = sender
+        self.receiver = receiver
+        self.messagetype = messagetype
+        self.dispatchtime = dispatchtime
+        self.extrainfo = extrainfo
+
+class EntityManager:
+    entityDictionary = {}
+
+    def registerEntity(self,newEntity):
+        self.entityDictionary[str(newEntity.id)] = newEntity
+
+    def getEntity(self,ID):
+        return self.entityDictionary[str(ID)]
+    
+    def deleteEntity(self,entity):
+        self.entityDictionary.pop(str(entity.id))
+
+class MessageDispatcher:
+    priorityQ  = []
+    
+    def Discharge(self,receiver,msg):
+        receiver.sendMessage(msg)
+
+    def DispatchMessage(self,delay,sender,receiver,msg,extraInfo):
+        message = Telegram(sender,receiver,msg,0,extraInfo)
+        receiverEntity = EntityManager.getEntity(EntityManager,receiver.ID)
+
+        if delay < 0:
+            self.Discharge(receiverEntity,message)
+        else:
+            message.dispatchtime = time.time + delay
+            self.priorityQ.append(message)
+
+    def DispatchDelayedMessages(self):
+        while priorityQ[0].dispatchtime < time.time and priorityQ[0].dispatchtime > 0:
+            
+            
+
+
+
+
+
 
 x = Miner(123)
+y = HouseWife(124)
 
 while True:
     x.Update()
-    time.sleep(3)
+    y.Update()
+    time.sleep(1)
+
