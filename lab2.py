9a3f2e477eb1167c48fbaf70f1dd4b9ec5653473
Zaimonius <simongus1@hotmail.com>
2020-02-27 17:30:14 +0100

    bfs done

--- lab2.py
+++ lab2.py
@@ -9,7 +9,7 @@ vec = pygame.math.Vector2
 
 
 class Grid: #https://www.youtube.com/watch?v=e3gbNOl4DiM
-    def __init__(self, gridwidth, gridheight, agentpos):
+    def __init__(self, gridwidth, gridheight, agentpos = None, goal = None):
         pygame.init()
         self.screen = pygame.display.set_mode((gridwidth * tilesize, gridheight * tilesize))
         self.clock = pygame.time.Clock()
@@ -20,19 +20,19 @@ class Grid: #https://www.youtube.com/watch?v=e3gbNOl4DiM
         #the walls of the map
         self.walls = []
         # the directions that the agent can move in
-        self.directions = [vec(1, 0), vec(0, -1), vec(-1, 0), vec(0, -1)]
+        self.directions = [vec(1, 0), vec(0, 1), vec(-1, 0), vec(0, -1)]
         self.running = False
-        self.player = agentpos
+        self.start = agentpos
+        self.goal = goal
         self.gridwidth = gridwidth
         self.gridheight = gridheight
 
         self.path = {}
         
-        icon_dir = path.join(path.dirname(__file__), '../icons')
         self.arrows = {}
         arrow_img = pygame.image.load('rightArrow.png').convert_alpha()
         arrow_img = pygame.transform.scale(arrow_img, (50, 50))
-        for dir in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+        for dir in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
             self.arrows[dir] = pygame.transform.rotate(arrow_img, vec(dir).angle_to(vec(1, 0)))
 
     def inBounds(self, node):
@@ -41,19 +41,24 @@ class Grid: #https://www.youtube.com/watch?v=e3gbNOl4DiM
     def passable(self, node):
         return node not in self.walls
 
-    def findNeighbors(self, node): #denna är fel atm
+    def findNeighbors(self, node):
         neighbors = []
         neighbors = [node + direction for direction in self.directions]
         neighbors = filter(self.inBounds, neighbors)
         neighbors = filter(self.passable, neighbors)
+        neighbors = list(neighbors)
         return neighbors
 
     def draw(self):
         for wall in self.walls:
             rect = pygame.Rect(wall * tilesize, (tilesize, tilesize))
             pygame.draw.rect(self.screen, lightgray, rect)
-        rect2 = pygame.Rect(self.player * tilesize, (tilesize, tilesize))
-        pygame.draw.rect(self.screen, red, rect2)
+        if self.start != None:
+            startRect = pygame.Rect(self.start * tilesize, (tilesize, tilesize))
+            pygame.draw.rect(self.screen, red, startRect)
+        if self.goal != None:
+            goalRect = pygame.Rect(self.goal * tilesize, (tilesize, tilesize))
+            pygame.draw.rect(self.screen, magenta, goalRect)
 
     def drawGrid(self):
         for x in range(0, self.width, tilesize):
@@ -69,6 +74,7 @@ class Grid: #https://www.youtube.com/watch?v=e3gbNOl4DiM
             for wall in wallPairs:
                 self.walls.append(vec(wall[0], wall[1]))
         f.close()
+
     @staticmethod
     def vec2int(vect):
         return (int(vect.x), int(vect.y))
@@ -77,28 +83,25 @@ class Grid: #https://www.youtube.com/watch?v=e3gbNOl4DiM
         frontier = deque()
         frontier.append(startNode)
         self.path = {}
-        path[self.vec2int(startNode)] = None
         while len(frontier) > 0:
             current = frontier.popleft()
-            for next in self.findNeighbors(current):
-                if self.vec2int(next) not in path:
-                    frontier.append(next)
-                    path[self.vec2int(next)] = current - next
+            if current == self.goal:
+                break
+            for nextNode in self.findNeighbors(current):
+                if self.vec2int(nextNode) not in self.path:
+                    frontier.append(self.vec2int(nextNode))
+                    self.path[self.vec2int(nextNode)] = current - nextNode
         print(path)
-        return path
+        return self.path
 
-
-    def run(self):
-        self.running = True
-        while self.running:
-            self.clock.tick(FPS)
-            for event in pygame.event.get():
+    def handleEvents(self):
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         self.running = False
-                    if event.key == pygame.K_s: #save map
+                    if event.key == pygame.K_q: #save map
                         f = open("drawnmap.txt", "w")
                         wallList = []
                         for wall in self.walls:
@@ -106,9 +109,15 @@ class Grid: #https://www.youtube.com/watch?v=e3gbNOl4DiM
                         f.write(str(wallList))
                         f.close()
                         print("map saved")
-                    if event.key == pygame.K_l:
+                    if event.key == pygame.K_w:
                         self.loadDrawn()
                         print("map loaded")
+                    if event.key == pygame.K_e:
+                        print("BFS!")
+                        self.path = self.breadthFirstSearch(self.start)
+                    if event.key == pygame.K_r:
+                        print("draw path!")
+                        #self.path = self.depthFirstSearch(self.start)
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     mousePos = vec(pygame.mouse.get_pos()) // tilesize
                     if event.button == 1:
@@ -116,28 +125,47 @@ class Grid: #https://www.youtube.com/watch?v=e3gbNOl4DiM
                             g.walls.remove(mousePos)
                         else:
                             g.walls.append(mousePos)
+                    if event.button == 2:
+                        self.goal = mousePos
                     if event.button == 3:
-                        start = mousePos
-                        self.path = self.breadthFirstSearch(start)
-            pygame.display.set_caption("{:.2f}".format(g.clock.get_fps()))
-            g.screen.fill(darkgray)
-            g.drawGrid()
-            g.draw()
+                        self.start = mousePos
 
-            for node, dir in self.path.items():
-                if dir:
+    def drawSearch(self):
+        for node, dire in self.path.items():
+                if dire:
                     x, y = node
                     x = x * tilesize + tilesize/2
                     y = y * tilesize + tilesize/2
-                    img = self.arrows[Grid.vec2int(dir)]
+                    img = self.arrows[Grid.vec2int(dire)]
                     r = img.get_rect(center=(x, y))
                     g.screen.blit(img, r)
 
+    def drawPath(self): # draws the path from start to goal
+        if len(self.path) != 0 and self.start != None:
+            current = self.start #the current node
+            while current != self.goal:
+                #stuff for drawing the arrows
+                x = current.x * tilesize + tilesize / 2
+                y = current.y * tilesize + tilesize / 2
+                img = self.arrows[self.vec2int(self.path[(current.x, current.y)])]
+                r = img.get_rect(center=(x, y))
+                g.screen.blit(img, r)
+                #find the next arrow in path
+                current = current + self.path[self.vec2int(current)]
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.clock.tick(FPS)
+            self.handleEvents()
+            pygame.display.set_caption("{:.2f}".format(self.clock.get_fps()))
+            self.screen.fill(darkgray)
+            self.drawGrid()
+            self.draw()
+            self.drawPath()
             pygame.display.flip()
 
 
-start = vec(5,5)
-g = Grid(10,10,start)
-g.findNeighbors(vec(0,0))
-g.loadDrawn()
+
+g = Grid(10,10)
 g.run()
